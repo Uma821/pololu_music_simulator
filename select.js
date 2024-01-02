b589d825d571011767096aedc7919e29ae2f365e
Uma821 <uma.ohmoto@gmail.com>
2024-01-02 19:59:30 +0900

    波形調整

--- select.js
+++ select.js
@@ -15,32 +15,32 @@ function make_chord(){
     });
     console.log(music_list);
 
-    var default_control_values = get_default_control_values();
+    var control_values = get_default_control_values();
     var time = {"4n":0}; // 4n: 4分音符
     return music_list.flatMap((tone) => {
         let regex_result
         if (regex_result = tone.match(/O(?<number>\d+)/i)) {
-            default_control_values.Octave = Number(regex_result.groups.number);
+            control_values.Octave = Number(regex_result.groups.number);
             return [];
         }
         if (regex_result = tone.match(/T(?<number>\d+)/i)) {
-            default_control_values.Tempo = Number(regex_result.groups.number);
+            control_values.Tempo = Number(regex_result.groups.number);
             return [];
         }
         if (regex_result = tone.match(/L(?<number>\d+)/i)) {
-            default_control_values.Duration = Number(regex_result.groups.number);
+            control_values.Duration = Number(regex_result.groups.number);
             return [];
         }
         if (regex_result = tone.match(/V(?<number>\d+)/i)) {
-            default_control_values.Volume = Number(regex_result.groups.number);
+            control_values.Volume = Number(regex_result.groups.number);
             return [];
         }
         if (tone == 'MS' || tone == 'ML') { // スタッカート or レガート
-            default_control_values.Articulation = tone;
+            control_values.Articulation = tone;
             return [];
         }
         if (regex_result = tone.match(/R(?<duration>\d*)(?<increasing>\.*)/i)) {
-            if (!regex_result.groups.duration) regex_result.groups.duration = default_control_values.Duration; // 省略されたときはデフォルト値
+            if (!regex_result.groups.duration) regex_result.groups.duration = control_values.Duration; // 省略されたときはデフォルト値
             regex_result.groups.duration = Number(regex_result.groups.duration);
 
             time[`${regex_result.groups.duration}n${regex_result.groups.increasing}`] ??= 0; // Null 合体代入
@@ -49,15 +49,15 @@ function make_chord(){
             return [];
         }
         if (regex_result = tone.match(/(?<octave>(>|<)*)(?<note>[A-G])(?<accidental>(\+|#|-)?)(?<duration>\d*)(?<increasing>\.*)/i)) {
-            if (!regex_result.groups.duration) regex_result.groups.duration = default_control_values.Duration; // 省略されたときはデフォルト値
+            if (!regex_result.groups.duration) regex_result.groups.duration = control_values.Duration; // 省略されたときはデフォルト値
             regex_result.groups.duration = Number(regex_result.groups.duration);
             regex_result.groups.accidental = regex_result.groups.accidental.replace('+', '#').replace('-', 'b');
 
             // console.log(regex_result.groups.octave.match(/>/g));
             let retval = ({
                 time:Object.assign({}, time), 
-                note:regex_result.groups.note+regex_result.groups.accidental+(default_control_values.Octave+(regex_result.groups.octave.match(/>/g)?.length??0)-(regex_result.groups.octave.match(/</g)?.length??0)), 
-                velocity: 0.9,
+                note:regex_result.groups.note+regex_result.groups.accidental+(control_values.Octave+(regex_result.groups.octave.match(/>/g)?.length??0)-(regex_result.groups.octave.match(/</g)?.length??0)), 
+                velocity: control_values.Volume / 15, // 音の強さ
                 duration: `${regex_result.groups.duration}n${regex_result.groups.increasing}`
             });
             time[retval.duration] ??= 0; // Null 合体代入演算子
@@ -82,12 +82,24 @@ async function play_chord() {
     await Tone.start();
     const chords = make_chord();
     console.log(chords);
-    Tone.Transport.bpm.value = 60; // 1分あたり60
+    // Tone.Transport.bpm.value = 60; // 1分あたり60
 
     [chords].forEach((chord) => {
         const part = new Tone.Part(((time, value) => {
-            const synth = new Tone.Synth().toDestination();
-            synth.triggerAttackRelease(value.note, value.duration, time, value.velocity);
+            const synth = new Tone.Synth({
+                oscillator: {
+                    // type: "triangle"
+                    // type: "sine"
+                    type: "square"
+                },
+                envelope: {
+                    attack : 0.005 , // second
+                    decay : 0.001 , // second
+                    sustain : 0.4 , // range [0, 1] ???
+                    release : 0 // second
+                }
+            }).toDestination();
+            synth.triggerAttackRelease(value.note, value.duration, time, value.velocity).unsync();
         }), chord).start();
     });
     Tone.Transport.start();
